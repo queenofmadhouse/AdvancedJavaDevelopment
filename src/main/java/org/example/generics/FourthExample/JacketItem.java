c2005b165f7847a6387a3e4331a3225d65828c5d
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-19 13:34:07 +0300

    demonstration of The Liskov substitution principle in Java created

---
+++ src/main/java/org/example/generics/FourthExample/JacketItem.java
@@ -0,0 +1,14 @@
+package org.example.generics.FourthExample;
+
+public class JacketItem extends ClothingItems {
+
+    @Override
+    int getPrice() {
+        return 55;
+    }
+
+    @Override
+    String getName() {
+        return "red jacket";
+    }
+}
---
+++ src/main/java/org/example/generics/FourthExample/JeansItem.java
@@ -0,0 +1,14 @@
+package org.example.generics.FourthExample;
+
+public class JeansItem extends ClothingItems{
+
+    @Override
+    int getPrice() {
+        return 31;
+    }
+
+    @Override
+    String getName() {
+        return "blue jeans";
+    }
+}
