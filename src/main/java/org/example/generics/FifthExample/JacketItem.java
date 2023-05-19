6cc8e67f010149cd44bdea966069a1777b3c1814
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-19 13:47:08 +0300

    wildcards in generic programming example (on base of fourth example) created

---
+++ src/main/java/org/example/generics/FifthExample/JacketItem.java
@@ -0,0 +1,14 @@
+package org.example.generics.FifthExample;
+
+public class JacketItem extends ClothingItems {
+
+    @Override
+    int getPrice() {
+        return 55;
+    }
+
+    @Override
+    String getName() {
+        return "red jacket";
+    }
+}
